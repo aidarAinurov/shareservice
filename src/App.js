6e2318c2567ec078cebf938b85dc0c21ca32b3c4
aidarainurov <ainur_1ai@mail.ru>
2021-02-28 20:45:56 +0300

    add links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import './App.css';
-import Header from './components/Header/index';
-import MainContent from './components/MainContent/index';
-import Footer from './components/Footer/index';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      <div>main page</div>
+      <nav>
+        <ul><Link to="/header">Header</Link></ul> 
+        <ul><Link to="/main">MainContent</Link> </ul>
+        <ul><Link to="/footer">Footer</Link> </ul>
+      </nav>
+      
       <Route path="/header"><Header /></Route>
       <Route path="/main"><MainContent /></Route>
       <Route path="/footer"><Footer /></Route>
--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import Dashboard from './Dashboard/index.jsx';
-import Home from './Home/index.jsx';
-import Profile from './Profile/index.jsx';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import Profile from './Profile';
 
 function MainContent() {
     return (
